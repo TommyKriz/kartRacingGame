5545648ed69abdb37ae5e90af9d6deddfa879b3e
TommyKriz <t@k.123>
2018-06-03 15:08:35 +0200

    wheel separation

--- src/main/java/tomcom/kartGame/systems/vehicle/VehicleGamepadInputDebugRendererSystem.java
+++ src/main/java/tomcom/kartGame/systems/vehicle/VehicleGamepadInputDebugRendererSystem.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class VehicleGamepadInputDebugRendererSystem extends IteratingSystem {
@@ -24,7 +25,7 @@ public class VehicleGamepadInputDebugRendererSystem extends IteratingSystem {
 
 	private static final int SEITENFUEHRUNGSKRAFT = 4000;
 
-	private static final float MAXIMUM_GAS_FORCE = -10000f;
+	private static final float MAXIMUM_GAS_FORCE = -30000f;
 
 	private static final Family FAMILY = Family.all(VehicleComponent.class,
 			Body2DComponent.class, GamepadInputComponent.class).get();
@@ -66,73 +67,92 @@ public class VehicleGamepadInputDebugRendererSystem extends IteratingSystem {
 		Body2DComponent chassis = bc.get(entity);
 
 		for (Controller controller : Controllers.getControllers()) {
-			Gdx.app.log("VehicleGamepadInputSystem", " axis 1 (LX): "
-					+ controller.getAxis(1));
 
-			for (Wheel w : vehicle.getSteerableWheels()) {
-				// TODO: +90?
-				w.orientation = controller.getAxis(1) * -MAXIMUM_WHEEL_ANGLE
-						+ 90;
-				w.getDirectionVector().setAngle(w.orientation);
-			}
+			turnWheels(vehicle, chassis, controller.getAxis(1));
 
-			Gdx.app.log("VehicleGamepadInputSystem", " axis 4 (LT RT): "
-					+ controller.getAxis(4));
+			sideForce(vehicle, chassis);
 
-			Vector2 wheelPivot;
-			for (Wheel w : vehicle.getWheels()) {
+			gas(vehicle, chassis, controller.getAxis(4));
 
-				wheelPivot = chassis.toWorldPoint(new Vector2(
-						w.xOffsetFromPivot, w.yOffsetFromPivot));
-
-				// visualize direction vector
-				drawVector(wheelPivot, w.getDirectionVector(),
-						DIRECTION_VECTOR_SCALE, Color.FIREBRICK);
-
-				Vector2 normalDirectionVector = w.getDirectionVector().cpy()
-						.rotate(90);
-				// visualize normal to direction vector
-				drawVector(wheelPivot, normalDirectionVector,
-						DIRECTION_VECTOR_SCALE, Color.YELLOW);
-
-				// visualize velocity
-				drawVector(wheelPivot, chassis.getVelocity(wheelPivot), 1,
-						Color.BLUE);
-
-				renderer.setColor(Color.GOLDENROD);
-				renderer.line(wheelPivot, chassis.getPosition());
-
-				float seitenfuehrungsSpeed = normalDirectionVector.dot(chassis
-						.getVelocity(wheelPivot));
-
-				Gdx.app.log("VehicleGameInputSystem", "SEITENFÜHRUNGSKRAFT: "
-						+ seitenfuehrungsSpeed);
-
-				Vector2 sideForce = normalDirectionVector.cpy().scl(
-						SEITENFUEHRUNGSKRAFT);
-				if (seitenfuehrungsSpeed < 0) {
-					// sideForce = normalDirectionVector;
-				} else if (seitenfuehrungsSpeed > 0) {
-					sideForce.scl(-1, -1);
-				} else {
-					sideForce = new Vector2(0, 0);
-				}
-				drawVector(wheelPivot, sideForce, 1, Color.PINK);
-
-				chassis.applyForce(sideForce, wheelPivot);
-
-				chassis.applyForce(
-						w.getDirectionVector().cpy()
-								.scl(controller.getAxis(4) * MAXIMUM_GAS_FORCE),
-						wheelPivot);
-			}
 		}
 
+	}
+
+	private void gas(VehicleComponent vehicle, Body2DComponent chassis,
+			float controllerYaxis) {
+		for (Wheel w : vehicle.getDrivenWheels()) {
+
+			chassis.applyForce(
+					w.getDirectionVector().cpy()
+							.scl(controllerYaxis * MAXIMUM_GAS_FORCE), chassis
+							.toWorldPoint(new Vector2(w.xOffsetFromPivot,
+									w.yOffsetFromPivot)));
+		}
+
+	}
+
+	private void sideForce(VehicleComponent vehicle, Body2DComponent chassis) {
+		Vector2 wheelPivot;
+		for (Wheel w : vehicle.getWheels()) {
+
+			wheelPivot = chassis.toWorldPoint(new Vector2(w.xOffsetFromPivot,
+					w.yOffsetFromPivot));
+
+			// visualize direction vector
+			drawVector(wheelPivot, w.getDirectionVector(),
+					DIRECTION_VECTOR_SCALE, Color.FIREBRICK);
+
+			Vector2 normalDirectionVector = w.getDirectionVector().cpy()
+					.rotate(90);
+			// visualize normal to direction vector
+			drawVector(wheelPivot, normalDirectionVector,
+					DIRECTION_VECTOR_SCALE, Color.YELLOW);
+
+			// visualize velocity
+			drawVector(wheelPivot, chassis.getVelocity(wheelPivot), 1,
+					Color.BLUE);
+
+			renderer.setColor(Color.GOLDENROD);
+			renderer.line(wheelPivot, chassis.getPosition());
+
+			float seitenfuehrungsSpeed = normalDirectionVector.dot(chassis
+					.getVelocity(wheelPivot));
+
+			Gdx.app.log("VehicleGameInputSystem", "SEITENFÜHRUNGSKRAFT: "
+					+ seitenfuehrungsSpeed);
+
+			Vector2 sideForce = normalDirectionVector.cpy().scl(
+					SEITENFUEHRUNGSKRAFT);
+			if (seitenfuehrungsSpeed < 0) {
+				// sideForce = normalDirectionVector;
+			} else if (seitenfuehrungsSpeed > 0) {
+				sideForce.scl(-1, -1);
+			} else {
+				sideForce = new Vector2(0, 0);
+			}
+			drawVector(wheelPivot, sideForce, 0.001f, Color.PINK);
+
+			chassis.applyForce(sideForce, wheelPivot);
+
+		}
+
+	}
+
+	private void turnWheels(VehicleComponent vehicle, Body2DComponent chassis,
+			float controllerXaxis) {
+		for (Wheel w : vehicle.getSteerableWheels()) {
+			// TODO: +90?
+			// TODO: take out chassis degrad
+			w.orientation = chassis.getAngleInRadians()
+					/ MathUtils.degreesToRadians + controllerXaxis
+					* -MAXIMUM_WHEEL_ANGLE + 90;
+			w.getDirectionVector().setAngle(w.orientation);
+		}
 	}
 
 	private void drawVector(Vector2 pivot, Vector2 v, float scale, Color color) {
 		renderer.setColor(color);
 		// TODO: v.cpy() is it too cost expensive
-		renderer.rectLine(pivot, pivot.cpy().add(v.scl(scale)), 0.025f);
+		renderer.rectLine(pivot, pivot.cpy().add(v.cpy().scl(scale)), 0.025f);
 	}
 }
