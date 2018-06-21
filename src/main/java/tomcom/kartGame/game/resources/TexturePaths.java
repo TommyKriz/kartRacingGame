d5b5d0adc72e7c291bc7e8f410ba98b34fb6796a
TommyKriz <lizardman420davidicke.com@gmail.com>
2018-06-21 21:26:27 +0200

    updated map

--- src/main/java/tomcom/kartGame/game/resources/TexturePaths.java
+++ src/main/java/tomcom/kartGame/game/resources/TexturePaths.java
@@ -4,5 +4,5 @@ public class TexturePaths {
 	protected static final String KART = "retro-red-car.png";
 	protected static final String WHEEL = "wheel.png";
 	protected static final String ROADBLOCK = "roadblock.png";
-	protected static final String MAP = "RaceTrackAll.png";
+	protected static final String MAP = "RaceTrack.png";
 }
