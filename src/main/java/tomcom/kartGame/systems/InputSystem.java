020c6b7c6a032df6ca8044851fb8bf4fdfb446f0
TommyKriz <lizardman420davidicke.com@gmail.com>
2018-07-02 14:47:53 +0200

    input system update

--- src/main/java/tomcom/kartGame/systems/InputSystem.java
+++ src/main/java/tomcom/kartGame/systems/InputSystem.java
@@ -8,33 +8,35 @@ import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.math.Vector2;
 
-public class InputSystem extends EntitySystem{
+public class InputSystem extends EntitySystem {
 
+	private static final float KEYBOARD_AXIS_SPEED = 0.8f;
 	public static Signal<Vector2> onInputReceived = new Signal<Vector2>();
+
 	@Override
 	public void update(float deltaTime) {
 		float xAxis = 0;
 		float yAxis = 0;
 		for (Controller controller : Controllers.getControllers()) {
 			xAxis = controller.getAxis(1);
-			yAxis= controller.getAxis(4);
+			yAxis = controller.getAxis(4);
 		}
-		if(Controllers.getControllers().size==0) {
-			
-			if(Gdx.input.isKeyPressed(Input.Keys.D)){
-				xAxis=-1;
+		if (Controllers.getControllers().size == 0) {
+
+			if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+				xAxis = KEYBOARD_AXIS_SPEED;
 			}
-			if(Gdx.input.isKeyPressed(Input.Keys.A)){
-				xAxis=1;
+			if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+				xAxis = -KEYBOARD_AXIS_SPEED;
 			}
-			if(Gdx.input.isKeyPressed(Input.Keys.W)){
-				yAxis=-1;
+			if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+				yAxis = -KEYBOARD_AXIS_SPEED;
 			}
-			if(Gdx.input.isKeyPressed(Input.Keys.S)){
-				yAxis=1;
+			if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+				yAxis = KEYBOARD_AXIS_SPEED;
 			}
 		}
 		onInputReceived.dispatch(new Vector2(xAxis, yAxis));
-		
+
 	}
 }
